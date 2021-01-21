77516641cd8ef3f19a0e5c3a03159f7dcd76753e
Tammy Chan <tammy.chan@genero.com>
2021-01-21 12:15:30 +1100

    add nav and children to layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,4 +1,12 @@
-const Layout = () => {
-  return <div>A layout</div>;
+import { FunctionComponent } from 'react';
+import Navigation from './Navigation/Navigation';
+
+const Layout: FunctionComponent = ({ children }) => {
+  return (
+    <div>
+      <Navigation />
+      {children}
+    </div>
+  );
 };
 export default Layout;
