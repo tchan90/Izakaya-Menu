242eb3d0f04772ed13dae3c6815e219c055eb818
Tammy Chan <tammy.chan@genero.com>
2021-01-21 12:15:55 +1100

    add fontawesome

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,8 @@
+import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
+import { config } from "@fortawesome/fontawesome-svg-core";
 import '../styles/globals.css';
+
+config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
 const MyApp = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
