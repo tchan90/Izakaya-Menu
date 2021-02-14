94bfd5ed9849210bd23a93117879e4e60f95082b
Tammy Chan <tammy.chan@genero.com>
2021-02-15 10:34:11 +1100

    add subtotal and total logic

--- modals/Cheque.tsx
+++ modals/Cheque.tsx
@@ -12,14 +12,17 @@ const Cheque = (props) => {
   const currentCart = useRecoilValue(kitchenState);
   let pricesArray = [];
   let total = 0;
-  currentCart.map((item) => {
-    pricesArray.push(item.price);
-  });
-  if (pricesArray.length !== 0) {
+  let itemTotal = 0;
+  if (currentCart.length > 0) {
+    currentCart.map((item) => {
+      itemTotal = item.price * item.count;
+      pricesArray.push(itemTotal);
+    });
     const addPrices = (acc, currentValue) => acc + currentValue;
     const getTotal = pricesArray.reduce(addPrices);
     total = getTotal;
   }
+
   const resetKitchen = useResetRecoilState(kitchenState);
   const readyToPay = () => {
     handleToggleModal();
@@ -28,6 +31,14 @@ const Cheque = (props) => {
 
   return (
     <ReactModal {...props}>
+      {currentCart.map((item) => (
+        <>
+          <p>
+            x{item.count} - {item.name}
+          </p>
+          <p>${item.price * item.count}</p>
+        </>
+      ))}
       <p>Total: ${total}</p>
       {total > 0 && <button onClick={readyToPay}>Ready to Pay</button>}
       <GoToCounter showModal={openModal} toggleModal={handleToggleModal} />
--- pages/order.tsx
+++ pages/order.tsx
@@ -26,8 +26,8 @@ const Order = () => {
         <Layout />
         <div className="bg-gray-200 h-screen mt-2 p-16 rounded-t-2xl">
           <h1 className="text-3xl text-center">Picked Orders</h1>
-          {cart.map((i) => (
-            <PickedOrders cartItems={i} />
+          {cart.map((i, k) => (
+            <PickedOrders cartItems={i} key={k} />
           ))}
           <div className="mt-52">
             <h1 className="text-3xl text-center mb-4">Confirmed Orders</h1>
