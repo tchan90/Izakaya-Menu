d6a2bc39a9baf7831705d25c3f7f5ef81cc35457
Tammy Chan <tammy.chan@genero.com>
2021-02-16 22:38:30 +1100

    border when cat selected

--- components/SideMenu/SideMenuCard.tsx
+++ components/SideMenu/SideMenuCard.tsx
@@ -1,9 +1,16 @@
+import { useRouter } from 'next/router';
 import Link from 'next/link';
 
 const SideMenuCard = ({ title }) => {
+  const router = useRouter();
+  const param = router.query.name;
   return (
     <Link href={`http://localhost:3000/menu/${title}`}>
-      <div className="w-full bg-gray-200 my-4 p-4 text-xl text-center capitalize cursor-pointer border-2 hover:border-indigo-500	">
+      <div
+        className={`w-full bg-gray-200 my-4 p-4 text-xl text-center capitalize cursor-pointer border-2 hover:border-indigo-500 ${
+          param === title && 'border-indigo-500'
+        }`}
+      >
         {title}
       </div>
     </Link>
