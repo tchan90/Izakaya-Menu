6be73d6b629e17035e8c0f6f353ec6bcc8c0aa1a
Tammy Chan <tammy.chan@genero.com>
2021-02-17 23:47:39 +1100

    add bg image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import Image from 'next/image';
 import { GetStaticProps } from 'next';
 
 export const getStaticProps: GetStaticProps = async (context) => {
@@ -16,13 +17,22 @@ export const getStaticProps: GetStaticProps = async (context) => {
 const Home = ({ menu }) => {
   const firstItem = Object.keys(menu.data)[0];
   return (
-    <div className="bg-gray-400 h-screen">
+    <div className="bg-black h-screen">
       <Head>
         <title>Izakaya Inn</title>
       </Head>
       <div className="flex h-screen">
+        <div className="h-full w-full bg-black bg-opacity-60 absolute z-10" />
+        <Image
+          src="https://source.unsplash.com/jfZfdQtcH6k/1600x900"
+          alt="Restaurant Home pic"
+          layout="fill"
+          objectFit="fill"
+        />
         <Link href={`http://localhost:3000/menu/${firstItem}`}>
-          <div className="m-auto cursor-pointer">Click to go to Menu</div>
+          <div className="text-7xl text-white font-serif border-double border-8 border-light-blue-500 m-auto px-9 py-5 cursor-pointer z-20 hover:bg-gray-600 hover:bg-opacity-70">
+            To Menu
+          </div>
         </Link>
       </div>
     </div>
