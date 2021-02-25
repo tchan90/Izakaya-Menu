1fe7e4c048136ee8a82924b917f8c427c5ae2112
Tammy Chan <tammy.chan@genero.com>
2021-02-25 11:17:19 +1100

    import tailwind into app directly

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '@fortawesome/fontawesome-svg-core/styles.css'; // import Font Awesome CSS
 import { config } from '@fortawesome/fontawesome-svg-core';
-import '../styles/compiledGlobal.css';
+import 'tailwindcss/tailwind.css';
+
 import { RecoilRoot } from 'recoil';
 
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
